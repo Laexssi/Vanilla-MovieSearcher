cdebe76ae5d75cd1e8f413fe439a785bcf1686e8
unknown <ktoornlaaldlaiepxxsi@gmail.com>
2019-08-15 17:18:08 +0300

    adjacent

--- script_fetch.js
+++ script_fetch.js
@@ -1,18 +1,20 @@
 const apiKey = '31a2acbd095355e3b5ac823a2a6f17a4';
 const searchForm = document.querySelector('#search-form');
 const searchText = document.querySelector('.form-control');
-const movie = document.querySelector('#movies');
+const movies = document.querySelector('#movies');
 const urlPoster = 'https://image.tmdb.org/t/p/w500';
-const movieBlock = document.querySelector('#container-movies')
+// const movieBlock = document.querySelector('#container-movies')
+
 const apiSearch = (e) => {
     e.preventDefault();
     const searchTextValue = searchText.value;
+    if (searchTextValue.trim().length === 0){
+        movies.insertAdjacentHTML('afterbegin', "<h2 class='col-12 text-center text-info'>Пустой запрос</h2>");
+        return;
+    }
     const server = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=ru&query=${searchTextValue}`;
-    movie.innerHTML = `<div class="d-flex justify-content-center">
-                            <div class="spinner-border" role="status">
-                                <span class="sr-only">Loading...</span>
-                            </div>
-                        </div>`;
+    movies.insertAdjacentHTML('afterbegin', '<div class="spinner"></div>');
+    // movies.innerHTML = '<div class="spinner"></div>';
 
     fetch(server)
         .then(result => {
@@ -22,33 +24,45 @@ const apiSearch = (e) => {
             return result.json()
         })
         .then(output => {
-            movie.innerHTML = "";
+            movies.innerHTML = "";
             
             console.log(output);
 
             if (output.results.length == 0) {
-                movie.innerHTML = "Ничего не найдено";
+                movies.innerHTML = `<h2 class='col-12 text-info text-center'>
+                    Ничего не найдено
+                    </h2>`;
             }
 
             output.results.forEach((item) => {
                 let nameItem = item.name || item.title;
-                const movieName = document.createElement("div");
+                const movieCard = document.createElement("div");
+                const movieName = document.createElement("h5");
                 const moviePoster = document.createElement("img");
+
                 item.poster_path ?
                 moviePoster.src = `${urlPoster + item.poster_path}`:
-                moviePoster.src = "https://filmitorrentom.org/films/noposter.jpg"
+                moviePoster.src = "https://filmitorrentom.org/films/noposter.jpg";
+
                 moviePoster.alt = `${nameItem}`;
                 moviePoster.classList.add("img-fluid");
-                movieName.classList.add("col-12", "col-md-4", "col-xl-3", "item");
-                movie.appendChild(movieName);
-                movieName.innerHTML = `<h5>${nameItem}</h5>`;
-                movieName.appendChild(moviePoster);
+
+                movieCard.classList.add("col-12", "col-md-4", "col-xl-3", "item");
+                movieName.innerHTML = nameItem;
+                movies.appendChild(movieCard);
+
+                movieCard.appendChild(movieName)
+                movieCard.appendChild(moviePoster);
             })
         })
-        .catch(reason => movie.innerHTML = `Ошибка: ${reason.status}`)
+        .catch(reason => movies.innerHTML = `Ошибка: ${reason.status}`)
 };
 
 
 
 
 searchForm.addEventListener('submit', apiSearch);
+
+function showInfo() {
+
+}
