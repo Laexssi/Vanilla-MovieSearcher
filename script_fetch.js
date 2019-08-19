2d57bda53cc4aee1ea1725fc48beec8e502a3317
Kirill <ktoornlaaldlaiepxssi@gmail.com>
2019-08-20 00:15:02 +0300

    scroll animation

--- script_fetch.js
+++ script_fetch.js
@@ -152,9 +152,15 @@ const isScroll = () => {
   : document.querySelector(".button-scroll").classList.remove("show")
 }
 
-const scrollToTop = () => {
-  
-  document.documentElement.scrollTop = 0;
+const scrollToTop = (scrollDuration) => {
+
+ var scrollStep = -window.scrollY / (scrollDuration / 15),
+        scrollInterval = setInterval(function(){
+        if ( window.scrollY != 0 ) {
+            window.scrollBy( 0, scrollStep );
+        }
+        else clearInterval(scrollInterval); 
+    },15);
 }
 
 window.onscroll = function() {isScroll()};
