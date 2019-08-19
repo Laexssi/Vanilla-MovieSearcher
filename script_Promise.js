02e67c2c013ffa0d339e699047908caf7655d194
unknown <ktoornlaaldlaiepxxsi@gmail.com>
2019-08-19 20:44:57 +0300

    scroll

--- script_Promise.js
+++ script_Promise.js
@@ -1,60 +1,154 @@
-const apiKey = '31a2acbd095355e3b5ac823a2a6f17a4';
-const searchForm = document.querySelector('#search-form');
-const searchText = document.querySelector('.form-control');
-const movie = document.querySelector('#movies');
+const apiKey = "31a2acbd095355e3b5ac823a2a6f17a4";
+const searchForm = document.querySelector("#search-form");
+const searchText = document.querySelector(".form-control");
+const movies = document.querySelector("#movies");
+const urlPoster = "https://image.tmdb.org/t/p/w500";
+const noPosterUrl = "https://filmitorrentom.org/films/noposter.jpg";
+let page = 1;
+let searchTextValue = "";
 
-const apiSearch = (e) => {
-    e.preventDefault();
-    const searchTextValue = searchText.value;
-    const server = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=ru&query=${searchTextValue}`;
-    movie.innerHTML = "Loading";
-    requestApi('GET', server)
-        .then(result => {
-            movie.innerHTML = "";
-            const output = result;
-            console.log(output);
 
-            if (output.results.length == 0) {
-                movie.innerHTML = "Ничего не найдено";
-            }
-            
-            output.results.forEach((item) => {
-                let nameItem = item.name || item.title;
-                const movieName = document.createElement("div");
-                movieName.classList.add("col-12", "col-md-4", "col-xl-3");
-                movie.appendChild(movieName);
-                movieName.innerHTML = nameItem;
-            })
-        })
-        .catch(reason => movie.innerHTML = `Ошибка: ${reason.status}`)
-        ;
+const apiSearch = e => {
+  e.preventDefault();
+  searchTextValue = searchText.value;
+  page = 1;
+  startIndex = 0;
+  if (searchTextValue.trim().length === 0) {
+    movies.insertAdjacentHTML(
+      "afterbegin",
+      "<h2 class='col-12 text-center text-info'>Empty input</h2>"
+    );
+    return;
+  }
+  const server = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=ru&query=${searchTextValue}`;
+  movies.insertAdjacentHTML("afterbegin", '<div class="spinner"></div>');
+  requestApi("GET", server)
+    .then(result => {
+      movies.innerHTML = "";
+      const output = result;
+      console.log(output);
 
+      if (output.results.length == 0) {
+        movies.insertAdjacentHTML(
+          "afterbegin",
+          "<h2 class='col-12 text-center text-info'>No results</h2>"
+        );
+      }
+
+      output.results.forEach((item, index) => {
+    
+          generateMovieCard(item, index);
+
+      });
+    })
+    .catch(reason => (movies.innerHTML = `Ошибка: ${reason.status}`));
 };
 
 const requestApi = (method, url) => {
+  return new Promise(function (resolve, reject) {
+    const request = new XMLHttpRequest();
 
-    return new Promise(function (resolve, reject) {
-        const request = new XMLHttpRequest();
+    request.open(method, url);
+    request.onload = () => {
+      if (request.status !== 200) {
+        reject({ status: request.status });
+        return;
+      }
 
-        request.open(method, url);
-        request.onload = () => {
-            if (request.status !== 200) {
-                reject({ status: request.status });
-                return;
-            }
+      resolve(request.response);
+    };
+    request.onerror = () => {
+      reject({ status: request.status });
+    };
 
-            resolve(request.response);
-        };
-        request.onerror = () => {
-            reject({ status: request.status })
-        };
-
-        request.responseType = "json";
-        request.send();
-
-    })
-
+    request.responseType = "json";
+    request.send();
+  });
 };
 
-searchForm.addEventListener('submit', apiSearch);
+const generateMovieCard = (item, index) => {
+  const movieCard = document.createElement('div');
+  movieCard.classList.add("col-12", "col-md-6", "col-xl-3", "item");
 
+
+
+  const movieName = document.createElement('h5');
+  const movieTitle = item.name || item.title;
+  movieName.innerHTML = movieTitle;
+
+
+  const moviePoster = document.createElement('img');
+  moviePoster.classList.add("img-fluid");
+  moviePoster.src = item.poster_path
+    ? posterUrl = `${urlPoster + item.poster_path}`
+    : posterUrl = noPosterUrl;
+  moviePoster.alt = movieTitle;
+
+  movies.appendChild(movieCard);
+  movieCard.appendChild(moviePoster);
+  movieCard.appendChild(movieName);
+
+
+  movieCard.setAttribute('index', index);
+  movieCard.setAttribute('page', page);
+
+
+
+  const templateButton = document.querySelector("#load-button-template")
+
+  const cloneTemplateButton = templateButton.content.cloneNode(true);
+
+
+  if (index >= 19) {
+    movies.appendChild(cloneTemplateButton);
+  }
+};
+
+const loadNextPage = () => {
+  page += 1;
+  console.log(page);
+ 
+  const server = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=ru&query=${searchTextValue}&page=${page}`;
+  const loadButton = document.querySelector('#load-button');
+
+  loadButton.insertAdjacentHTML("beforebegin", '<div class="spinner"></div>');
+
+  requestApi("GET", server)
+    .then(result => {
+      const spinner = document.querySelector(".spinner");
+      loadButton.remove();
+      spinner.remove();
+      const output = result;
+      console.log(output);
+
+      if (output.results.length == 0) {
+        movies.insertAdjacentHTML(
+          "afterbegin",
+          "<h2 class='col-12 text-center text-info'>No more movies</h2>"
+        );
+        }
+      output.results.forEach((item, index) => {
+        generateMovieCard(item, index);
+    });
+    })
+};
+
+
+
+searchForm.addEventListener("submit", apiSearch);
+
+
+const isScroll = () => {
+
+  
+  ( document.documentElement.scrollTop > 500) 
+  ? document.querySelector(".button-scroll").classList.add("show")
+  : document.querySelector(".button-scroll").classList.remove("show")
+}
+
+const scrollToTop = () => {
+  
+  document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = function() {isScroll()};
--- script_fetch.js
+++ script_fetch.js
@@ -1,54 +1,94 @@
-const apiKey = '31a2acbd095355e3b5ac823a2a6f17a4';
-const searchForm = document.querySelector('#search-form');
-const searchText = document.querySelector('.form-control');
-const movie = document.querySelector('#movies');
-const urlPoster = 'https://image.tmdb.org/t/p/w500';
-const movieBlock = document.querySelector('#container-movies')
-const apiSearch = (e) => {
-    e.preventDefault();
-    const searchTextValue = searchText.value;
-    const server = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=ru&query=${searchTextValue}`;
-    movie.innerHTML = `<div class="d-flex justify-content-center">
-                            <div class="spinner-border" role="status">
-                                <span class="sr-only">Loading...</span>
-                            </div>
-                        </div>`;
+const apiKey = "31a2acbd095355e3b5ac823a2a6f17a4";
+const searchForm = document.querySelector("#search-form");
+const searchText = document.querySelector(".form-control");
+const movies = document.querySelector("#movies");
+const urlPoster = "https://image.tmdb.org/t/p/w500";
+// const movieBlock = document.querySelector('#container-movies')
 
-    fetch(server)
-        .then(result => {
-            if (result.status !== 200) {
-                return Promise.reject(result)
-            }
-            return result.json()
-        })
-        .then(output => {
-            movie.innerHTML = "";
-            
-            console.log(output);
+const apiSearch = e => {
+  e.preventDefault();
+  const searchTextValue = searchText.value;
+  if (searchTextValue.trim().length === 0) {
+    movies.insertAdjacentHTML(
+      "afterbegin",
+      "<h2 class='col-12 text-center text-info'>Пустой запрос</h2>"
+    );
+    return;
+  }
+  const server = `https://api.themoviedb.org/3/search/multi/?api_key=${apiKey}&language=ru&query=${searchTextValue}`;
+  movies.insertAdjacentHTML("afterbegin", '<div class="spinner"></div>');
+  // movies.innerHTML = '<div class="spinner"></div>';
 
-            if (output.results.length == 0) {
-                movie.innerHTML = "Ничего не найдено";
-            }
+  fetch(server, {
+    method: "GET",
+  mode: 'no-cors',
+    headers: {
+      "Content-Type": "application/json",
+     
+    },
+    xhrFields: {
+      withCredentials: false
+    }
+  })
+    .then(result => {
+        console.log(result);
+      if (result.status !== 200) {
+        return Promise.reject(result);
+      }
+      return result.json();
+    })
+    .then(output => {
+      movies.innerHTML = "";
+      console.log(output);
 
-            output.results.forEach((item) => {
-                let nameItem = item.name || item.title;
-                const movieName = document.createElement("div");
-                const moviePoster = document.createElement("img");
-                item.poster_path ?
-                moviePoster.src = `${urlPoster + item.poster_path}`:
-                moviePoster.src = "https://filmitorrentom.org/films/noposter.jpg"
-                moviePoster.alt = `${nameItem}`;
-                moviePoster.classList.add("img-fluid");
-                movieName.classList.add("col-12", "col-md-4", "col-xl-3", "item");
-                movie.appendChild(movieName);
-                movieName.innerHTML = `<h5>${nameItem}</h5>`;
-                movieName.appendChild(moviePoster);
-            })
-        })
-        .catch(reason => movie.innerHTML = `Ошибка: ${reason.status}`)
+      if (output.results.length == 0) {
+        movies.innerHTML = `<h2 class='col-12 text-info text-center'>
+                    Ничего не найдено
+                    </h2>`;
+      }
+
+      output.results.forEach((item, index) => {
+        try {
+          const movieTemplate = document.querySelector("#movies-template");
+          const cloneMovieCard = movieTemplate.content.cloneNode(true);
+
+          movies.appendChild(cloneMovieCard);
+
+          const movieCard = document.querySelectorAll("#movie-card"),
+            movieName = document.querySelectorAll("#movie-title"),
+            moviePoster = document.querySelectorAll("#movie-poster");
+
+          console.log(movieCard);
+
+          let nameItem = item.name || item.title;
+          let posterUrl;
+
+          item.poster_path
+            ? (posterUrl = `${urlPoster + item.poster_path}`)
+            : (posterUrl = "https://filmitorrentom.org/films/noposter.jpg");
+
+          movieName[index].innerHTML = nameItem;
+          moviePoster[index].src = posterUrl;
+          moviePoster[index].alt = nameItem;
+          moviePoster[index].classList.add("img-fluid");
+          movieCard[index].classList.add(
+            "col-12",
+            "col-md-4",
+            "col-xl-3",
+            "item"
+          );
+
+          if (index >= 19) {
+            movies.appendChild
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      });
+    })
+    .catch(reason => (movies.innerHTML = `Ошибка: ${reason.status}`));
 };
 
+searchForm.addEventListener("submit", apiSearch);
 
-
-
-searchForm.addEventListener('submit', apiSearch);
+function showInfo() {}
